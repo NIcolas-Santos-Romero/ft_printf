4478a29b96db1754931d992706181a8e066f151a
Nicolas WSL <nicolas.santos200601@gmail.com>
2026-06-25 21:53:59 -0300

    Refactor: Remove all functions and structures from libft
    - Deleted all function implementations from the libft library, including memory management, string manipulation, and linked list functions.
    - Removed the libft.h header file which contained function prototypes and type definitions.
    - Added a new header file for ft_printf with basic includes for future implementation.

--- ft_printf.c
+++ ft_printf.c
@@ -1,7 +1,7 @@
-#include " ft_printf.h"
+#include "ft_printf.h"
 
 
-static ft_con(const char type, va_list)
+static int ft_con(const char type, va_list)
 {
 
 }
@@ -10,11 +10,14 @@ int ft_printf(const char *str, ...)
 {
 	va_list	args;
 	int		i;
+
+	if (!str)
+		return (NULL);
 	va_start(args, str);
 	while (str[i] != "\0")
 	{
 		if (str[i] == "%")
-
+		i += ft_con(str[i + 1], args);
 	}
 
 
